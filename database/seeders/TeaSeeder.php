d399bfae800228d54497c86fc328f2e759059851
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-20 12:21:14 +0200

    Test decimal number

--- database/seeders/TeaSeeder.php
+++ database/seeders/TeaSeeder.php
@@ -19,7 +19,7 @@ class TeaSeeder extends Seeder
             'type' => "rooibos",
             'region' => "South Africa",
             'ingredients' => "<p> Rooibos is a herbal infusion. Unlike real tea, rooibos does not contain caffeine. It does contain antioxidants, including vitamin C, as well as calcium and iron. According to some people, the drink has a slightly relaxing effect. Rooibos would be good before bedtime. The drink is also said to help against various ailments, such as hay fever, asthma, allergies, stomach complaints, acne and eczema. In South Africa, cold rooibos herbal tea is given to babies who suffer from intestinal cramps. <p>",
-            'price' => '0'
+            'price' => 4.85,
         ]);
     }
 }
