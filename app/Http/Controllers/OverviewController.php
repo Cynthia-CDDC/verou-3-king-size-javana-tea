be158e8ef499d2355a952b92ed47c1bcaf1f3881
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-20 13:43:25 +0200

    Add controller + test database info

---
+++ app/Http/Controllers/OverviewController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tea;
+
+class OverviewController extends Controller
+{
+    public function overviewTeas()
+    {
+        $tea = Tea::all();
+        dd($tea);
+        return view('home');
+    }
+}
