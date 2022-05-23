e6aaadf95783882958e73bffac584a893706567b
Cynthia <cynthia.decelle@gmail.com>
2022-05-23 10:49:02 +0200

    Add relationships to Tea model

--- app/Models/Tea.php
+++ app/Models/Tea.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Tea extends Model
 {
     use HasFactory;
+
+    public function users_teas()
+    {
+        return $this->hasMany(UserTea::class);
+    }
+    
+    public function teas_characteristics()
+    {
+        return $this->hasMany(TeaCharacteristic::class);
+    }
 }
