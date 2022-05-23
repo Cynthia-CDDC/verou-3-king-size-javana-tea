eabcf66badbedfd4fa6ef67a12dc3e884e85941a
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-23 16:03:15 +0200

    Delete unecessary code

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,9 +16,3 @@ mix.js("resources/js/app.js", "public/js").postCss(
     "public/css",
     [require("postcss-import"), require("tailwindcss"), require("autoprefixer")]
 );
-
-mix.webpackConfig({
-    stats: {
-        children: true,
-    },
-});
