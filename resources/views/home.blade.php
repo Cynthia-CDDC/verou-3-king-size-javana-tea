c806d369ee9fb9138c576500f7bbe589944e8f64
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-24 11:37:57 +0200

    Layout styling homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
-    <section class="w-4/5 mx-auto">
+    <section class="w-4/5 mx-auto flex flex-row flex-wrap justify-between gap-12">
         @foreach ($teas as $tea)
-            <h2 class="text-red font-bold">
-                {{ $tea->name }}
-            </h2>
-            <div>
-                <img src="{{ asset('images/' . $tea->image) }}" alt="photo natural rooibos" height="100" width="120" />
-            </div>
-            <span>&euro; {{ $tea->price }}</span>
+            <article>
+                <div>
+                    <img src="{{ asset('images/' . $tea->image) }}" alt="photo natural rooibos" class="h-72 w-72" />
+                </div>
+                <h2 class="text-red font-bold">
+                    {{ $tea->name }}
+                </h2>
+                <span>&euro; {{ $tea->price }}</span>
+            </article>
         @endforeach
     </section>
 @endsection
