1e9a6077121c3c9d5649d4ad7d67cbfc8ad91563
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-24 11:07:18 +0200

    Add basic styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
-    <section>
+    <section class="w-4/5 mx-auto">
         @foreach ($teas as $tea)
-            <h2>
+            <h2 class="text-red font-bold">
                 {{ $tea->name }}
             </h2>
-            <p>
-                {{ $tea->type }}
-            </p>
             <div>
                 <img src="{{ asset('images/' . $tea->image) }}" alt="photo natural rooibos" height="100" width="120" />
             </div>
+            <span>&euro; {{ $tea->price }}</span>
         @endforeach
     </section>
 @endsection
