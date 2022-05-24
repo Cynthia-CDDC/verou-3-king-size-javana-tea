f885bb32fa18795c740bbe5c4b8108179270b828
Cynthia <cynthia.decelle@gmail.com>
2022-05-24 17:17:20 +0200

    Create characteristicTeaSeeder

--- database/seeders/CharacteristicTeaSeeder.php
+++ database/seeders/CharacteristicTeaSeeder.php
@@ -30,5 +30,89 @@ class CharacteristicTeaSeeder extends Seeder
             'characteristic_id' => '9',
             'tea_id' => '1'
         ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '1',
+            'tea_id' => '2'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '2',
+            'tea_id' => '2'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '3',
+            'tea_id' => '2'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '4',
+            'tea_id' => '3'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '7',
+            'tea_id' => '3'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '2',
+            'tea_id' => '4'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '3',
+            'tea_id' => '4'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '4',
+            'tea_id' => '4'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '8',
+            'tea_id' => '4'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '2',
+            'tea_id' => '5'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '7',
+            'tea_id' => '5'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '4',
+            'tea_id' => '5'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '9',
+            'tea_id' => '6'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '8',
+            'tea_id' => '6'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '2',
+            'tea_id' => '7'
+        ]);
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '9',
+            'tea_id' => '7'
+        ]);
+
+        $characteristicTea = \App\Models\CharacteristicTea::create([
+            'characteristic_id' => '4',
+            'tea_id' => '7'
+        ]);
     }
 }
