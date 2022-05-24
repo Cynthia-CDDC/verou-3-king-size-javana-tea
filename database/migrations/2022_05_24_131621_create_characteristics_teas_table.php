1288af4989534447b8df846798d2c1659cf337a5
Cynthia <cynthia.decelle@gmail.com>
2022-05-24 15:25:24 +0200

    Create new migration for new table characteristics_teas

---
+++ database/migrations/2022_05_24_131621_create_characteristics_teas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characteristics_teas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('tea_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characteristics_teas');
+    }
+};
