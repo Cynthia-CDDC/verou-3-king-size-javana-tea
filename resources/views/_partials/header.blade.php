651572094d78334ce223ea929026f7459eed6a5b
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-24 13:51:03 +0200

    Add styling header + route

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,14 +1,19 @@
-<header>
-    <nav class="flex justify-end">
-        <ul class="flex mx-5 gap-3">
+<header class="h-24 bg-orange-700 flex items-center justify-end">
+    <nav>
+        <ul class="flex mx-5 gap-3 text-neutral-50 font-bold">
             <li>
-                <a href="#">Home</a>
+                <a href="{{ route('home') }}"
+                    class="relative after:hover:top-6 after:hover:left-1/2 after:hover:absolute after:hover:rounded-full after:hover:w-2 after:hover:h-2 after:hover:bg-neutral-50">Home</a>
             </li>
             <li>
-                <a href="#">My Collection</a>
+                <a href="#"
+                    class="relative after:hover:top-6 after:hover:left-1/2 after:hover:absolute after:hover:rounded-full after:hover:w-2 after:hover:h-2 after:hover:bg-neutral-50">My
+                    Collection</a>
             </li>
             <li>
-                <a href="{{ route('login') }}">Log In</a>
+                <a href="{{ route('login') }}"
+                    class="relative after:hover:top-6 after:hover:left-1/2 after:hover:absolute after:hover:rounded-full after:hover:w-2 after:hover:h-2 after:hover:bg-neutral-50">Log
+                    In</a>
             </li>
         </ul>
     </nav>
