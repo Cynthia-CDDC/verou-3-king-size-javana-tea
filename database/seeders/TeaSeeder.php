9a41e909bd0cc56db66d509d2262c600cde51395
Cynthia <cynthia.decelle@gmail.com>
2022-05-30 14:12:15 +0200

    Eliminate p tags in tables

--- database/seeders/TeaSeeder.php
+++ database/seeders/TeaSeeder.php
@@ -18,7 +18,7 @@ class TeaSeeder extends Seeder
             'name' => 'Natural rooibos',
             'type' => "rooibos",
             'region' => "South Africa",
-            'ingredients' => "<p> Rooibos is a herbal infusion. Unlike real tea, rooibos does not contain caffeine. It does contain antioxidants, including vitamin C, as well as calcium and iron. According to some people, the drink has a slightly relaxing effect. Rooibos would be good before bedtime. The drink is also said to help against various ailments, such as hay fever, asthma, allergies, stomach complaints, acne and eczema. In South Africa, cold rooibos herbal tea is given to babies who suffer from intestinal cramps. <p>",
+            'ingredients' => "Rooibos is a herbal infusion. Unlike real tea, rooibos does not contain caffeine. It does contain antioxidants, including vitamin C, as well as calcium and iron. According to some people, the drink has a slightly relaxing effect. Rooibos would be good before bedtime. The drink is also said to help against various ailments, such as hay fever, asthma, allergies, stomach complaints, acne and eczema. In South Africa, cold rooibos herbal tea is given to babies who suffer from intestinal cramps.",
             'price' => 4.80,
             'image' => 'rooibos-natuur4-1636988943.jpg'
         ]);
@@ -27,7 +27,7 @@ class TeaSeeder extends Seeder
             'name' => 'Strawberry Kiwi',
             'type' => "fruit infusion",
             'region' => "",
-            'ingredients' => "<p>Fruit blend with rosehip, hibiscus, strawberry & kiwi<p>",
+            'ingredients' => "Fruit blend with rosehip, hibiscus, strawberry & kiwi",
             'price' => 5.60,
             'image' => 'aardbei-kiwi-1615642937.jpg'
         ]);
@@ -36,7 +36,7 @@ class TeaSeeder extends Seeder
             'name' => 'Amaretto tea',
             'type' => "black tea",
             'region' => "",
-            'ingredients' => "<p>lack tea with almond pieces and amaretto aroma.<p>",
+            'ingredients' => "lack tea with almond pieces and amaretto aroma.",
             'price' => 5.50,
             'image' => 'javana2020-by-as-deldycke-193-1615739290.jpg'
         ]);
@@ -45,7 +45,7 @@ class TeaSeeder extends Seeder
             'name' => 'Flower garden',
             'type' => "green tea",
             'region' => "China",
-            'ingredients' => "<p>Green Sencha tea with pieces of papaya, pineapple, strawberry and raspberry blended with malven flowers, rose petals<p>",
+            'ingredients' => "Green Sencha tea with pieces of papaya, pineapple, strawberry and raspberry blended with malven flowers, rose petals",
             'price' => 5.80,
             'image' => 'flower-garden-z-blik-1636121244.jpg'
         ]);
@@ -54,7 +54,7 @@ class TeaSeeder extends Seeder
             'name' => 'Ginger Love',
             'type' => "herbal infusion",
             'region' => "",
-            'ingredients' => "<p>Herbal blend with ginger, cinnamon, blueberry leaf, apple, sugar, bamboo leaf, orange blossom, rosebuds. Anti-inflammatory and digestive.<p>",
+            'ingredients' => "Herbal blend with ginger, cinnamon, blueberry leaf, apple, sugar, bamboo leaf, orange blossom, rosebuds. Anti-inflammatory and digestive.",
             'price' => 6.30,
             'image' => 'ginger-love-1615739723.jpg'
         ]);
@@ -63,7 +63,7 @@ class TeaSeeder extends Seeder
             'name' => 'Formosa oolong',
             'type' => "black tea oolong tea",
             'region' => "Taïwan",
-            'ingredients' => "<p>Formosa is the former name of Taiwan. Oolong tea is semi-oxidized tea which is therefore neither black nor green. Due to its low caffeine content, the tea does not become bitter and can be drunk throughout the day. A beautiful leaf tea with a fruity, refined taste.<p>",
+            'ingredients' => "Formosa is the former name of Taiwan. Oolong tea is semi-oxidized tea which is therefore neither black nor green. Due to its low caffeine content, the tea does not become bitter and can be drunk throughout the day. A beautiful leaf tea with a fruity, refined taste.",
             'price' => 5.30,
             'image' => 'formosa-oolong-1625564222.jpg'
         ]);
@@ -72,7 +72,7 @@ class TeaSeeder extends Seeder
             'name' => 'Snow white',
             'type' => "white tea",
             'region' => "China",
-            'ingredients' => "<p>Blend of Pai mu tan white tea, sencha, red currant, strawberry, peony & rosebuds<p>",
+            'ingredients' => "Blend of Pai mu tan white tea, sencha, red currant, strawberry, peony & rosebuds",
             'price' => 6.20,
             'image' => 'javana2020-by-as-deldycke-193-1615739290.jpg'
         ]);
