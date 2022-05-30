bb339b1d5dc1108f6df60a4767e38498c398825f
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-30 15:01:58 +0200

    Change footer content colour

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="mt-auto flex justify-center items-center bg-orange-700 h-16">
-    <p>
+    <p class="text-neutral-50">
         &copy; Cynthia Decelle &#38; Michelle Radomski
     </p>
 </footer>
