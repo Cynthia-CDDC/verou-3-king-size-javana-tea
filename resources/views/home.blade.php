e5556c931b052ed7fe9303418495f23091745fef
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-31 15:45:37 +0200

    Change layout + styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="flex p-5">
-        <aside class="w-1/5">
-            <ul>
+    <div class="flex p-5 justify-between">
+        <aside class="w-1/6">
+            <h2 class="pb-2 border-b border-orange-800 text-orange-800 font-bold">Kind</h2>
+            <ul class="pt-2">
                 @foreach ($characteristics as $characteristic)
-                    <li>
-                        <input type="checkbox" id="{{ $characteristic->name }}" name="{{ $characteristic->name }}">
-                        <label for="{{ $characteristic->name }}">{{ $characteristic->name }}</label>
+                    <li class="flex items-center gap-3 hover:cursor-pointer">
+                        <input type="checkbox" id="{{ $characteristic->name }}" name="{{ $characteristic->name }}"
+                            class="hover:cursor-pointer">
+                        <label for="{{ $characteristic->name }}"
+                            class="hover:cursor-pointer">{{ $characteristic->name }}</label>
                     </li>
                 @endforeach
             </ul>
         </aside>
         <main class="w-4/5">
-            <section class="flex flex-wrap justify-between gap-y-5">
+            <section class="flex flex-wrap justify-end gap-10 p-2">
                 @foreach ($teas as $tea)
                     <article>
                         <div class="overflow-hidden">
