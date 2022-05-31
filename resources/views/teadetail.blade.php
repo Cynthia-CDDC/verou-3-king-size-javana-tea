81f1b926326148662935dbf38f8324b7e25c3711
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-31 11:30:15 +0200

    Remove duplicate code

--- resources/views/teadetail.blade.php
+++ resources/views/teadetail.blade.php
@@ -8,27 +8,15 @@
                 <p><i>{{ $tea->type }}</i></p>
                 <p class="py-4">{{ $tea->ingredients }}</p>
                 <p class="text-amber-600">&euro; {{ $tea->price }}</p>
+                <ul>
+                    @foreach ($tea->teasCharacteristics as $characteristic)
+                        <li>{{ $characteristic->name }}</li>
+                    @endforeach
+                </ul>
             </div>
             <div>
                 <img src="{{ asset('images/' . $tea->image) }}" alt="Picture of tea" class="w-72 h-72 order-1">
             </div>
         </section>
     </main>
-    <section class="flex justify-center w-4/5">
-        <div class="order-2 mx-5 w-3/5">
-            <h1 class="text-red font-bold">{{ $tea->name }}</h1>
-            <p><i>{{ $tea->type }}</i></p>
-            <p class="py-4">{{ $tea->ingredients }}</p>
-            <p>{{ $tea->price }}</p>
-
-            <ul>
-                @foreach ($tea->teasCharacteristics as $characteristic)
-                    <li>{{ $characteristic->name }}</li>
-                @endforeach
-            </ul>
-        </div>
-        <div>
-            <img src="{{ asset('images/' . $tea->image) }}" alt="Picture of tea" class="w-52 h-52 order-1">
-        </div>
-    </section>
 @endsection
