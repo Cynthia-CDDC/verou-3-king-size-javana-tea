04dd9b85f787bcebf8e2fdba735f8597ad79d58f
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-31 11:20:42 +0200

    Add main tag

--- resources/views/teadetail.blade.php
+++ resources/views/teadetail.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
-    <section class="flex justify-center w-4/5">
-        <div class="order-2 mx-5 w-3/5">
-            <h1 class="text-red font-bold">{{ $tea->name }}</h1>
-            <p><i>{{ $tea->type }}</i></p>
-            <p class="py-4">{{ $tea->ingredients }}</p>
-            <p class="text-amber-600">&euro; {{ $tea->price }}</p>
-        </div>
-        <div>
-            <img src="{{ asset('images/' . $tea->image) }}" alt="Picture of tea" class="w-72 h-72 order-1">
-        </div>
-    </section>
+    <main class="py-5 flex justify-center">
+        <section class="flex justify-center w-4/5">
+            <div class="order-2 mx-5 w-3/5">
+                <h1 class="text-red font-bold">{{ $tea->name }}</h1>
+                <p><i>{{ $tea->type }}</i></p>
+                <p class="py-4">{{ $tea->ingredients }}</p>
+                <p class="text-amber-600">&euro; {{ $tea->price }}</p>
+            </div>
+            <div>
+                <img src="{{ asset('images/' . $tea->image) }}" alt="Picture of tea" class="w-72 h-72 order-1">
+            </div>
+        </section>
+    </main>
 @endsection
