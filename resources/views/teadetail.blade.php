291970147c48b95c85d405ae0129ea0da3b723b8
Michelle Radomski <michi.radomski@hotmail.com>
2022-05-31 11:02:28 +0200

    Make img bigger + change colour

--- resources/views/teadetail.blade.php
+++ resources/views/teadetail.blade.php
@@ -6,10 +6,10 @@
             <h1 class="text-red font-bold">{{ $tea->name }}</h1>
             <p><i>{{ $tea->type }}</i></p>
             <p class="py-4">{{ $tea->ingredients }}</p>
-            <p>{{ $tea->price }}</p>
+            <p class="text-amber-600">&euro; {{ $tea->price }}</p>
         </div>
         <div>
-            <img src="{{ asset('images/' . $tea->image) }}" alt="Picture of tea" class="w-52 h-52 order-1">
+            <img src="{{ asset('images/' . $tea->image) }}" alt="Picture of tea" class="w-72 h-72 order-1">
         </div>
     </section>
 @endsection
