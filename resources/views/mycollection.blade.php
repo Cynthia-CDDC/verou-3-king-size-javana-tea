1c662cbde4727a4187afd0643563a8d2e1ff2c83
Michelle Radomski <michi.radomski@hotmail.com>
2022-06-03 13:50:18 +0200

    Add + style basic mycollection view

---
+++ resources/views/mycollection.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.layout')
+
+@section('content')
+    <main class="py-5 flex flex-col ">
+        <section class="flex justify-start w-4/5 mx-auto">
+            <h1>
+                Like
+            </h1>
+        </section>
+        <section class="flex justify-start w-4/5 mx-auto">
+            <h1>
+                Dislike
+            </h1>
+        </section>
+        <section class="flex justify-start w-4/5 mx-auto">
+            <h1>
+                Want to try
+            </h1>
+        </section>
+    </main>
+@endsection
